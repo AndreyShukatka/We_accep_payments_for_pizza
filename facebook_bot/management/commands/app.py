a20e2fc763c1740586f70936473c654b9214a1a4
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-04-26 11:54:02 +0300

    Add Facebook webhook

--- facebook_bot/management/commands/app.py
+++ facebook_bot/management/commands/app.py
@@ -10,21 +10,19 @@ class Command(BaseCommand):
     help = 'Вебхуки для Facebook'
 
     app = Flask(__name__)
-
     @app.route('/', methods=['GET'])
-    def verify(self):
+    def verify():
         """
         При верификации вебхука у Facebook он отправит запрос на этот адрес. На него нужно ответить VERIFY_TOKEN.
         """
         if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-            if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            if not request.args.get("hub.verify_token") == settings.FACEBOOK_TOKEN:
                 return "Verification token mismatch", 403
             return request.args["hub.challenge"], 200
-
         return "Hello world", 200
 
     @app.route('/', methods=['POST'])
-    def webhook(self):
+    def webhook():
         """
         Основной вебхук, на который будут приходить сообщения от Facebook.
         """
@@ -36,26 +34,27 @@ class Command(BaseCommand):
                         sender_id = messaging_event["sender"]["id"]
                         recipient_id = messaging_event["recipient"]["id"]
                         message_text = messaging_event["message"]["text"]
-                        self.send_message(sender_id, message_text)
+                        send_message(sender_id, message_text)
         return "ok", 200
 
-    def send_message(recipient_id, message_text):
-        params = {"access_token": settings.FACEBOOK_TOKEN}
-        headers = {"Content-Type": "application/json"}
-        request_content = {
-            "recipient": {
-                "id": recipient_id
-            },
-            "message": {
-                "text": message_text
-            }
-        }
-        response = requests.post(
-            "https://graph.facebook.com/v2.6/me/messages",
-            params=params, headers=headers, json=request_content
-        )
-        response.raise_for_status()
 
     def handle(self, *args, **options):
-        app = Flask(__name__)
-        app.run(debug=True)
+        self.app.run(debug=True)
+
+
+def send_message(recipient_id, message_text):
+    params = {"access_token": settings.FACEBOOK_TOKEN}
+    headers = {"Content-Type": "application/json"}
+    request_content = {
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    }
+    response = requests.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params=params, headers=headers, json=request_content
+    )
+    response.raise_for_status()
